91eaeb2df7d2aaab5429d08dae4e42f3ba5864a6
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-01-19 22:11:07 +0300

    create struct

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
 import(
-	""
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
 )
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title`
+	Director *Director`json:"director"`
+
+}
+
+type Director struct{
+	FirstName string `json:firstname`
+	LatsName string `json:lastname`
+
+}
+
+var movies []Movie
+
+func main()  {
+	r := mux.NewRouter()
+	r. func
+	
+}
