1b2363018d4241724f1330395ef71d36b7c465a5
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-01-20 13:20:41 +0300

    create movies

--- main.go
+++ main.go
@@ -1,28 +1,57 @@
 package main
 
-import (
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
-type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title`
-	Director *Director `json:"director"`
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title`
+	Director *Director`json:"director"`
+
 }
 
-type Director struct {
+type Director struct{
 	FirstName string `json:firstname`
-	LatsName  string `json:lastname`
+	LastName string `json:lastname`
+
 }
 
 var movies []Movie
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/(id)", getMovie).Methods("GET")
-	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/(id)", updateMovie).Methods("PUT")
+func getMovies(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+	
+}
 
+func deleteMovie(w http.ResponseWriter,  r *http.Request)  {
+	w.Header().Set("content.Type", "application/json")
+	params :=
+}
+
+
+
+func main()  {
+	r := mux.NewRouter()
+
+	movies =append(movies,Movie{ID:"1", Isbn: "43288", Title: "Movie One", Director: &Director{FirstName: "john", LastName: "Doe"}} )
+	movies =append(movies,Movie{ID:"2", Isbn: "45425", Title: "Movie Two", Director: &Director{FirstName: "Steve", LastName: "Smith"}} )
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting a server at port 8000/n")
+	log.Fatal(http.ListenAndServe(":8000",r))
+	
 }
